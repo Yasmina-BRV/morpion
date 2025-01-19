6011b04892a7d271d3478e835ca8d09c033ea43d
Yasmina-BRV <yasminal.bravo@hotmail.com>
2025-01-19 22:32:16 +0100

    add winner features

--- src/components/Boards/Board.tsx
+++ src/components/Boards/Board.tsx
@@ -1,19 +1,36 @@
+import { Button } from "flowbite-react";
 import Square from "../Squares/Square";
 
 type BoardProps = {
     squares: TSquare[];
     squareClick: (index: number) => void;
+    winner: string;
+    reset: () => void;
+    player: string;
 }
 
 export type TSquare = "X" | "O" | null
 
 const Board = (props:BoardProps) => {
     return (
-        <div className="grid grid-cols-3 grid-rows-3 relative gap-4 max-w-xl mx-auto">
-            {props.squares.map((square, index) => (
-                <Square square={square} onClick={() => props.squareClick(index)} />
-            ))}
-        </div>
+        <>
+            
+            {props.winner && (
+                <div className="flex justify-center">
+                <h3 className="flex justify-center mb-2">
+                    {props.winner === "draw" ? "C'est un match nul" : `Joueur ${props.winner} à gagné!`}
+                </h3>
+                </div>
+            )}
+            <div className="flex justify-center">
+                <Button onClick={props.reset} type="submit" className="bg-pale-grey shadow-lg shadow-pale-grey-500/50 hover:bg-medium-grey mb-4">Reset</Button>
+            </div>
+            <div className="grid grid-cols-3 grid-rows-3 relative gap-4 max-w-xl mx-auto mb-4">
+                {props.squares.map((square, index) => (
+                    <Square square={square} onClick={() => props.squareClick(index)} />
+                ))}
+            </div>
+        </>
     );
 };
 export default Board;
--- src/pages/Game.tsx
+++ src/pages/Game.tsx
@@ -1,10 +1,10 @@
-import { useState } from "react";
-import Board from "../components/Boards/Board"
+import { useEffect, useState } from "react";
+import Board, { TSquare } from "../components/Boards/Board"
 
 const Game = () => {
     const [squares, setSquares] = useState(Array(9).fill(null));
-
     const [player, setPlayer] = useState("X");
+    const [winner, setWinner] = useState("");
 
     const handleSquareClick = (index: number) => {
         // Return if square already contents a value
@@ -18,17 +18,56 @@ const Game = () => {
         setPlayer(player === "X" ? "O" : "X");
     };
 
+    const checkWinner = (squares:TSquare[]) => {
+        const combos = [
+            [0, 1, 2], //1ère ligne
+            [3, 4, 5], //2ème ligne 
+            [6, 7, 8], //3ème ligne
+            [0, 3, 6], //diagonale de la gauche vers le bas
+            [1, 4, 7], //diagonale de la droite vers le bas
+            [2, 5, 8], //1ère colonne
+            [0, 4, 8], //2ème colonne
+            [2, 4, 6], //3ème colonne
+        ];
+
+        for(let i = 0; i < combos.length; i++) {
+            const [a, b, c] = combos[i];
+            if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+                if (squares[a] === "X") {
+                    setWinner("X");
+                } else {
+                    setWinner("O");
+                }
+                return;
+            }
+        }
+        const isDraw = squares.every((squares) => squares !== null);
+        setWinner(isDraw ? "draw" : null);
+    };
+
+    useEffect(() => {
+        checkWinner(squares);
+    }, [squares]);
+    
+    const handleReset = () => {
+        if (!winner && winner !== "draw") return;
+        setSquares(Array(9).fill(null));
+        setPlayer("X");
+        setWinner("");
+    };
+
     return (
         <>
             <h1 className="my-4 flex justify-center text-2xl font-bold">Morpion</h1>
 
             <div className="flex flex-row justify-center gap-4 my-4">
-                <div className="bg-pale-grey size-16 rounded text-center">Joueur 1</div>
+                <div className="bg-pale-grey size-16 rounded text-center">Joueur 1 X</div>
                 <div className="bg-pale-grey size-16 rounded text-center">Match nul</div>
-                <div className="bg-pale-grey size-16 rounded text-center">Joueur 2</div>
+                <div className="bg-pale-grey size-16 rounded text-center">Joueur 2 O</div>
             </div>
 
-            <Board squares={squares} squareClick={handleSquareClick} />
+
+            <Board squares={squares} squareClick={handleSquareClick} player={player} winner={winner} reset={handleReset} />
         </>
     )
     
